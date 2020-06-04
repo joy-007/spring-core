b2a01e23b848d4cf9cbeeb65d5e6bf0f4de2e6a3
souvik <souvik.jgec13@gmail.com>
2020-06-04 19:21:54 +0530

    changes

---
+++ MyApp.java
@@ -0,0 +1,13 @@
+package com.souvik.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		
+		/*Coach bsCoach = new BaseballCoach();
+		System.out.println(bsCoach.getDailyWorkOut());
+*/
+	}
+
+}
